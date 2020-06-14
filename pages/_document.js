b177ad7a7715c96a98d863f260e5e70677b60eb6
Ragnor Comerford <hello@ragnor.co>
2020-06-15 00:09:11 +0200

    Remove segment.

--- pages/_document.js
+++ pages/_document.js
@@ -3,26 +3,11 @@
 
 // ./pages/_document.js
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import * as snippet from "@segment/snippet";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
-  }
-  renderSnippet() {
-    const opts = {
-      apiKey: process.env.SEGMENT_KEY,
-      // note: the page option only covers SSR tracking.
-      // Page.js is used to track other events using `window.analytics.page()`
-      page: true,
-    };
-
-    if (process.env.NODE_ENV === "development") {
-      return snippet.max(opts);
-    }
-
-    return snippet.min(opts);
   }
 
   render() {
