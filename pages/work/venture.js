b3073cd8df6c971a20b34c3905c66762cd6edc67
Fynn Comerford <comerford.fynn@yahoo.de>
2020-08-18 18:31:08 +0000

    j

--- pages/work/venture.js
+++ pages/work/venture.js
@@ -38,7 +38,7 @@ export default () => {
               <strong>Location:</strong> Edinburgh, UK
             </p>
             <p>
-              <strong>Website: </strong>{"www.edventure.vc"}
+              <strong>Website: </strong> href="www.edventure.vc" {"www.edventure.vc"}
            
             </p>
          
