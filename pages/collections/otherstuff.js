a9824fd032498b9e168f80012fcb2b5cb254f7d0
Fynn Comerford <comerford.fynn@yahoo.de>
2020-08-28 11:37:12 +0000

    w

--- pages/collections/otherstuff.js
+++ pages/collections/otherstuff.js
@@ -49,7 +49,7 @@ export default () => {
                   
 
                   <div className="border-l-2 h-10 border-black mx-5 rounded-xl"></div>
-                  <h3 className="text-lg">Poems:</h3>
+                  <h3 className="text-lg ml-5">Poems:</h3>
                   <br />
                   <ul>
                   <li> Desiderata </li> 
