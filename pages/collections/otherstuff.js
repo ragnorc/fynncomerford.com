d35b3536786e5167c0efedfc0a03577f69f7ace5
Fynn Comerford <comerford.fynn@yahoo.de>
2020-09-06 10:17:00 +0000

    h

--- pages/collections/otherstuff.js
+++ pages/collections/otherstuff.js
@@ -1,86 +1,47 @@
-import Masonry from "react-masonry-css";
 import Layout from "components/layout";
-import Image from "components/image";
-import Link from "next/link";
+import { request } from "graphql-request";
 
-export default () => {
+export default ({ items, heading }) => {
+  console.log(items);
   return (
     <Layout>
-      <div className="pl-18 lg:pl-32 w-full leading-relaxed">
-        <h1 className="text-5xl font-bold mb-12">
-          <span className="text-primary">o</span>ther stuff
-        </h1>
-        <p> Here are some other, more general things that I am a big fan of. </p>
-        <br />
-        <br />
-        <br />
+      <div className="pl-18 lg:pl-32 w-2/1 leading-relaxed">
+        <h2>Online courses</h2>
+        <ul>
+        <li>
+        <a target="_blank" className="">
         
-        <Masonry
-          className="w-auto flex"
-          columnClassName="px-5"
-          breakpointCols={{
-            default: 3,
-            1150: 2,
-            700: 1,
-            400: 1,
-          }}
-        >
-          
-            <a>
-              <div className="shadow-black rounded-lg bg-gray-100 mb-6">
-                <div className="flex w-full justify-center items-center h-48 xxl:h-64">
-                  
-                  <div className="border-l-2 h-10 border-black mx-5 rounded-xl"></div>
-                  <h3 className="text-lg">Online courses:</h3>
-                  <br />
-              
-                   The Science of Wellbeing, Yale
-                                    
-                  
-                </div>
-              </div>
-            </a>
-          
-          
-            <a>
-              <div className="shadow-black bg-gray-100 rounded-lg  mb-6">
-                <div className="flex w-full justify-center items-center h-48 xxl:h-64">
-                  
-
-                  <div className="border-l-2 h-10 border-black mx-5 rounded-xl"></div>
-                  <h3 className="text-lg"> Poems:</h3>
-                  <br />
-                  
-                   Desiderata 
-                    <br />
-                   Do Not Stand At My Grave               
-                    <br />
-                   Do Not Go Gentle 
-                  
-                </div>
-              </div>
-            </a>
-         
-          
-            <a>
-              <div className="shadow-black rounded-lg mb-6 bg-gray-100">
-                <div className="flex w-full justify-center items-center h-48 xxl:h-64">
-                  
-
-                  <div className="border-l-2 h-10 border-black mx-5"></div>
-                  <h3 className="text-lg"> Artists:</h3>
-                  <br />
-                  
-Véronique Ziminski 
-              
-                </div>
-              </div>
-            </a>
-          
-              
-            
-        </Masonry>
+          <span class="italic"> The Science of Wellbeing, </span>
+Yale
+          <br />
+        </a>
+        </li>
+        <li>
+        <a target="_blank" className="">
+        
+          <span class="italic"> Python for Science, </span>
+Harvardx
+          <br />
+        </a>
+        </li>
+       
+        </ul>
+        <h2>Artists</h2>
+        <ul>
+        <li>
+        <a target="_blank" className="">
+          <span class="italic">Véronique Ziminski</span>
+          </li>
+        </ul>
+        <h2>Poems</h2>
+        <ul>
+        <li>
+         Desiderata </li>
+       <li>  Do Not Go Gentle </li>
+        <li> Do Not Stand By My Grave And Weep </li>
+        </ul>
       </div>
+    
     </Layout>
   );
 };
