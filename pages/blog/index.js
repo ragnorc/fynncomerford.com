32a6747cc187fdda2cc3f2c9d221ed701b6d5257
Fynn Comerford <comerford.fynn@yahoo.de>
2020-10-01 13:58:44 +0000

    c

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -33,7 +33,10 @@ export default () => {
           <h1 className="text-5xl font-bold mb-12">
             <span className="text-primary">b</span>log
           </h1>
-        <div id="retainable-rss-embed" 
+        
+        </div>
+      </div>
+<div id="retainable-rss-embed" 
 data-rss="https://medium.com/feed/@fynncomerford"
 data-maxcols="2" 
 data-layout="grid" 
@@ -43,9 +46,6 @@ data-readmore="Read the rest">
 data-buttonclass="btn btn-primary" 
 data-offset="-100"></div>
 <script src="https://www.retainable.app/assets/retainable/rss-embed/retainable-rss-embed.js"></script>
-        </div>
-      </div>
-
     </Layout>
   );
 };
