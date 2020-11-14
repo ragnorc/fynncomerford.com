9e1d2c4dc04348fd79adb051a896266c9fbdab18
Fynn Comerford <comerford.fynn@yahoo.de>
2020-11-14 17:43:09 +0000

    d

--- pages/work/igem.js
+++ pages/work/igem.js
@@ -95,7 +95,7 @@ Stripping back to the bare necessities for function also provides a safer altern
 </p>
        
 
-        <iframe src="https://uoe-my.sharepoint.com/personal/s1811556_ed_ac_uk/_layouts/15/Doc.aspx?sourcedoc={e0c5eff6-bc3f-4a97-9cdb-f03464307c0a}&amp;action=embedview&amp;wdAr=1.7777777777777777" width="350px" height="221px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+       <iframe src="https://uoe-my.sharepoint.com/personal/s1811556_ed_ac_uk/_layouts/15/Doc.aspx?sourcedoc={e0c5eff6-bc3f-4a97-9cdb-f03464307c0a}&amp;action=embedview&amp;wdAr=1.7777777777777777" width="1186px" height="691px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
     
      
       </div>
