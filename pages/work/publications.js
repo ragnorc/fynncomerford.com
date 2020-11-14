4d3d2ed73647b8b9bfb725efe1b8e7cc3843556a
Fynn Comerford <comerford.fynn@yahoo.de>
2020-11-14 11:02:04 +0000

    d

--- pages/work/publications.js
+++ pages/work/publications.js
@@ -32,7 +32,7 @@ export default () => {
                 <div className="flex w-full justify-center items-center h-48 xxl:h-64">
                   <img className="w-1/6 h-auto " src="/img/illustrations/medicalresearch.svg" />
                   <div className="border-l-2 h-10 border-black mx-5 rounded-xl"></div>
-                  <h6 className="text-lg">Comerford, F., Comerford, R., Hussain Z., Ng, N., Hussain, A., Khan, A., Lees, C. A <span className="italic"> Comparison of Machine Learning Approaches for Predicting the Progression of Crohn's Disease</span></h6>
+                  <h6 href="https://www.researchgate.net/publication/345848669_A_Comparison_of_Machine_Learning_Approaches_for_Predicting_the_Progression_of_Crohn's_Disease"className="text-lg">Comerford, F., Comerford, R., Hussain Z., Ng, N., Hussain, A., Khan, A., Lees, C. A., "A Comparison of Machine Learning Approaches for Predicting the Progression of Crohn's Disease"<span className="italic">SCOReD</span>Batu Pahat, Johor, Malaysia, 2020, pp. 529-533, doi: 10.1109/SCOReD50371.2020.9251019.</h6>
                 </div>
                 </div>
                 
