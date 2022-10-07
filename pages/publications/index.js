26da10e0adf9b79dd3a052b6c195c74c4233ce13
Fynn Comerford <53836849+FynnCOM@users.noreply.github.com>
2022-10-07 02:35:37 +0000

    ss

--- pages/publications/index.js
+++ pages/publications/index.js
@@ -26,7 +26,32 @@ export default () => {
       <h4>Here is an overview of some research papers,   articles and essays I have published.</h4>
       <br /> 
       <br />
+
+       <h2>Papers</h2>
+      <br />
+    <h6  className="text-lg">Comerford, F., Comerford, R., Hussain Z., Ng, N., Hussain, A., Khan, A., Lees, C. A., "A Comparison of Machine Learning Approaches for Predicting the Progression of Crohn's Disease"<span className="italic">, SCOReD</span>, Batu Pahat, Johor, Malaysia, 2020, pp. 529-533, <a href="https://ieeexplore.ieee.org/document/9251019">doi: 10.1109/SCOReD50371.2020.9251019.</a></h6>
+    
+    
+              <br />
+
+      
+              
+              <p>You can also find my full ResearchGate profile <a class="underline" href="https://www.researchgate.net/profile/Fynn_Comerford"> here.</a></p>
+
+       <h2>Theses</h2> 
+      <a 
+      class="underline"
+    href="https://www.researchgate.net/publication/363919098_Can_Zebrafish_Bridge_the_Treatment_Gap_in_Progressive_Multiple_Sclerosis_An_optimized_assessment_of_toxicity_and_efficacy_of_candidate_pharmacotherapies?channel=doi&linkId=6334f03d769781354ea38d05&showFulltext=true">Can Zebrafish Bridge the Treatment Gap in Progressive Multiple Sclerosis? An optimized assessment of toxicity and efficacy of candidate pharmacotherapies.</a>
+      
+
+
+      
       <h2>Essays</h2> 
+
+      <a 
+      class="underline"
+    href="https://fynncomerford.medium.com/predictive-coding-a-unified-theory-of-cognition-in-health-and-disease-afcb4523e525">Predictive Coding: A Unified Theory of Cognition in Health and Disease?</a>
+      <br />
       <a 
       class="underline"
     href="https://fynncomerford.medium.com/are-animals-moral-beings-an-essay-68ce12c6c3c0">Are animals moral beings?</a>
@@ -38,21 +63,7 @@ export default () => {
       <h2>Posters</h2>
       <a class= "underline" href="https://drive.google.com/file/d/11pI3xHfw2ijk41HA1A4g1BU3hG9AnzWS/view?usp=sharing">How Pilocarpine and Tropicamide affect visual accommodation and pupil diameter</a>
 
-      <h2>Research</h2>
-      <br />
-    
-      <div className="shadow-black rounded-lg bg-gray-100 mb-6">
-                <div className="flex w-full justify-center items-center h-48 xxl:h-64">
-                  <img className="w-1/6 h-auto " src="/img/illustrations/medicalresearch.svg" />
-                  <div className="border-l-2 h-10 border-black mx-5 rounded-xl"></div>
-                  <h6  className="text-lg">Comerford, F., Comerford, R., Hussain Z., Ng, N., Hussain, A., Khan, A., Lees, C. A., "A Comparison of Machine Learning Approaches for Predicting the Progression of Crohn's Disease"<span className="italic">, SCOReD</span>, Batu Pahat, Johor, Malaysia, 2020, pp. 529-533, <a href="https://ieeexplore.ieee.org/document/9251019">doi: 10.1109/SCOReD50371.2020.9251019.</a></h6>
-                </div>
-                </div>
-                
-            
-              <br />
-              
-              <p>You can also find my full ResearchGate profile <a class="underline" href="https://www.researchgate.net/profile/Fynn_Comerford"> here.</a></p>
+     
        </Layout>         
               
               
