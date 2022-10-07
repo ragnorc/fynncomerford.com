3575bf8905ece4cfb1ead86079169a86d249e9a3
Fynn Comerford <53836849+FynnCOM@users.noreply.github.com>
2022-10-07 00:40:53 +0000

    e

--- pages/about.js
+++ pages/about.js
@@ -87,23 +87,17 @@ export default () => {
 
 
         <TimelineItem
-          left
+          right
           image="/img/illustrations/2020.svg"
           minWidth="auto"
           text={<p>In my 3rd year, I started <a target="https://edventure.vc/" className="underline">edventure,  </a> Europe's biggest student-run accelerator.</p>} 
         />
           
 <TimelineItem
-          right
-          image="/img/illustrations/medicalresearch.svg"
-          minWidth="auto"
-          text={<p>During the pandemic, I also participated in the synthetic biology competition iGEM where we created a cell-free <a href="/work/igem" className="underline">biosensor.</a> </p>} 
-        />
-<TimelineItem
           left
           image="/img/illustrations/medicalresearch.svg"
           minWidth="auto"
-          text={<p>During the pandemic, I also participated in the synthetic biology competition iGEM where we created a cell-free <a href="/work/igem" className="underline">biosensor.</a> </p>} 
+          text={<p>In 2020, I also took part in<a href="/work/igem" className="underline">iGEM</a>.</p>} 
         />
 
         <TimelineItem
@@ -111,7 +105,7 @@ export default () => {
           image="/img/illustrations/empty.svg"
           minWidth="auto"
           maxWidth="auto"
-          text={<p>During the challenging pandemic, I interned in a VC firm, collaborated on two research papers and founded an accelerator. Check out my recent projects <a href="/work" className="underline">here. </a></p>} 
+          text={<p>During the pandemic, I started some exciting <a href="/work" className="underline">projects</a>.</p>} 
         />
 
 
@@ -119,38 +113,37 @@ export default () => {
           left
           image="/img/illustrations/video.svg"
           minWidth="auto"
-          text={<p>Meanwhile, I developed a great passion for filmmaking. You can see some of my clips <a href="/work/films" className="underline"s>here.</a></p>} 
+          text={<p>Meanwhile, I developed a great passion for filmmaking. <a href="/work/films" className="underline"s>Here's</a> what I've done.</p>} 
         />
        
         <TimelineItem
           right
           image="/img/illustrations/doctor.svg"
-          text="2019 For the second year of my studies, I transferred to the University of Edinburgh."
+          text="In 2019 I transferrred to study Neuroscience at the University of Edinburgh."
         />
           <TimelineItem
             left
             image="/img/illustrations/exam.svg"
             minWidth="auto"
-            text="In 2018, I started my first year of Neuroscience at the University of Manchester. 
+            text="In 2018, I started studying Neuroscience at the University of Manchester. 
           />
            <TimelineItem
           right
           image="/img/illustrations/plane.svg"
           minWidth="auto"
-          text={<p>After wrapping up Magious, I travelled through Europe, Asia and South America. I wrote about some of my experiences <a href="/work/blog" className="underline"s>here.</a></p>} 
+          text={<p>Before university, I also travelled through South America and Asia.</p>} 
         />
           <TimelineItem
             left
             image="/img/illustrations/magiousbook.svg"
             minWidth="auto"
-            text={<p>2017 Magious: After graduating a 
-              publishing company contracted the startup I had founded in highschool. <a href="/work/magious" className="underline">Find out more.</a></p>}
+            text={<p>I, then,started my first company -<a href="/work/magious" className="underline">Magious.</a></p>}
           />
           <TimelineItem
             right
             image="/img/illustrations/graduate.svg"
             minWidth="auto"
-            text="2017 High school: I graduated as the top student of my year with a 4.0 GPA [1.0 Abitur]."
+            text="In 2017 I graduated from high school"
           />
         
       </div>
